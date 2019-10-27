36751643572c410a937e52d61e1816da4b7d74d9
Guido <guidodizi@gmail.com>
2019-10-27 20:06:02 -0300

    Restructure async processing. Mongo to save timestamps

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const helmet = require("helmet");
 const controller = require('./controller')
 const rateLimit = require("express-rate-limit");
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO, {useNewUrlParser: true});
 
 const app = express();
 const limiter = rateLimit({
@@ -15,57 +18,62 @@ app.use(cors());
 app.use(helmet());
 app.use(limiter);
 
-const extendTimeoutMiddleware = (req, res, next) => {
-  const space = ' ';
-  let isFinished = false;
-  let isDataSent = false;
-  let count = 0;
-
-  res.once('finish', () => {
-    isFinished = true;
-  });
-
-  res.once('end', () => {
-    isFinished = true;
-  });
-
-  res.once('close', () => {
-    isFinished = true;
-  });
-
-  res.on('data', (data) => {
-    // Look for something other than our blank space to indicate that real
-    // data is now being sent back to the client.
-    console.log('data event')
-    if (data !== space) {
-      console.log('data event end')
-      isDataSent = true;
-    }
-  });
-
-  const waitAndSend = () => {
-    setTimeout(() => {
-      // If the response hasn't finished and hasn't sent any data back....
-      if (!isFinished && !isDataSent) {
-        res.write(space);
-        count++;
-
-        // wait for 150sec max
-        if (count <= 10){
-          // Wait another 15 seconds
-          waitAndSend();
-        }
-      }
-    }, 15000);
-  };
-
-  waitAndSend();
-  next();
-};
-
-app.use(extendTimeoutMiddleware);
 app.get('/run', controller.run)
+app.get('/check', controller.check)
 
 app.listen(process.env.PORT || 5000, () =>
   console.log(`Server up & running on port: ${process.env.PORT || 5000}`)
 );
+
+
+
+
+// const extendTimeoutMiddleware = (req, res, next) => {
+//   const space = ' ';
+//   let isFinished = false;
+//   let isDataSent = false;
+//   let count = 0;
+
+//   res.once('finish', () => {
+//     isFinished = true;
+//   });
+
+//   res.once('end', () => {
+//     isFinished = true;
+//   });
+
+//   res.once('close', () => {
+//     isFinished = true;
+//   });
+
+//   res.on('data', (data) => {
+//     // Look for something other than our blank space to indicate that real
+//     // data is now being sent back to the client.
+//     console.log('data event')
+//     if (data !== space) {
+//       console.log('data event end')
+//       isDataSent = true;
+//     }
+//   });
+
+//   const waitAndSend = () => {
+//     setTimeout(() => {
+//       // If the response hasn't finished and hasn't sent any data back....
+//       if (!isFinished && !isDataSent) {
+//         res.write(space);
+//         count++;
+
+//         // wait for 150sec max
+//         if (count <= 10){
+//           // Wait another 15 seconds
+//           waitAndSend();
+//         }
+//       }
+//     }, 15000);
+//   };
+
+//   waitAndSend();
+//   next();
+// };
+
+// app.use(extendTimeoutMiddleware);
